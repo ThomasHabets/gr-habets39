2f3f28767714f3c90ce274c7e2886d8826d499b3
Thomas Habets <thomas@habets.se>
2023-10-13 23:39:50 +0100

    build.sh: Hide my own install dir behind secret flag
    Fixes #2

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-set -e
+
+set -ueo pipefail
+
 cd "$(dirname "$0")"
 rm -fr build
 mkdir build
 cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$HOME/opt/gnuradio-3.9.4.0 \
-    -DCMAKE_BUILD_TYPE=Release \
-    ..
-exec make -j$(nproc)
+
+if [[ $1 = "thomas" ]]; then
+    cmake \
+	-DCMAKE_INSTALL_PREFIX=$HOME/opt/gnuradio-3.9.4.0 \
+	-DCMAKE_BUILD_TYPE=Release \
+	..
+else
+    cmake \
+	..
+fi
+make -j$(nproc)
+echo "Now 'cd build && make install' to install"
